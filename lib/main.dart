77e49cfa8c405a65fc9debb6d6990e98e0eee3f0
User <shounbabu31@gmail.com>
2025-11-18 20:16:34 +0600

    update

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,4 @@
-import 'package:flutter/cupertino.dart';
+
 import 'package:flutter/material.dart';
 import 'package:get/get_navigation/src/root/get_material_app.dart';
 
@@ -11,7 +11,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-
 
 
     );
