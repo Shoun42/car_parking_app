bc5f7631b14857d25ebbf64e25f320fa73058c0a
User <shounbabu31@gmail.com>
2025-11-21 21:19:31 +0600

    update

--- lib/view/screen/common_screen/splash/splash_screen.dart
+++ lib/view/screen/common_screen/splash/splash_screen.dart
@@ -6,7 +6,8 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("He"),),
+      appBar: AppBar(title: Text("Hello"),),
+
     );
   }
 }
